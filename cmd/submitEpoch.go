f8b854f57048f898838b1adac8e48e7bfe83d77b
Marten Sigwart <marten@okamaja.de>
2020-01-09 11:27:25 +0100

    add --json flag If this flag is set, the command generate epoch data for the specified epoch but instead of submitting it to the Ethash smart contract, it saves the date in JSON format to a file with the name <epoch>.json

--- cmd/submitEpoch.go
+++ cmd/submitEpoch.go
@@ -4,13 +4,18 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/pantos-io/go-testimonium/ethereum/ethash"
+	"github.com/pantos-io/go-testimonium/typedefs"
 	"math/big"
+	"os"
 
 	"log"
 
 	"github.com/spf13/cobra"
 )
+
+var jsonFlag bool
 
 // submitEpochCmd represents the command for setting epoch data (Ethash contract)
 var submitEpochCmd = &cobra.Command{
@@ -27,14 +32,23 @@ var submitEpochCmd = &cobra.Command{
 			log.Fatalf("Illegal epoch number '%s'", args[0])
 		}
 
-		testimoniumClient = createTestimoniumClient()
 		epochData := ethash.GenerateEpochData(epoch.Uint64())
+
+		if jsonFlag {
+			writeEpochAsJson(epochData, epoch)
+			fmt.Printf("Wrote epoch data to %s.json\n", epoch.String())
+			return
+		}
+		testimoniumClient = createTestimoniumClient()
 		testimoniumClient.SetEpochData(epochData, submitFlagDestChain)
 	},
 }
 
+
 func init() {
 	submitCmd.AddCommand(submitEpochCmd)
+
+	submitEpochCmd.Flags().BoolVar(&jsonFlag, "json", false, "creates a JSON file containing the epoch data without submitting it")
 
 	// Here you will define your flags and configuration settings.
 
@@ -45,4 +59,42 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// disputeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+
+func writeEpochAsJson(epochData typedefs.EpochData, epoch *big.Int) {
+	f, err := os.Create(fmt.Sprintf("./%s.json", epoch.String()))
+	checkError(err)
+	defer f.Close()
+
+	_, err = fmt.Fprint(f, "{\n")
+	_, err = fmt.Fprintf(f, "  \"epoch\": \"%s\",\n", epochData.Epoch.String())
+	_, err = fmt.Fprintf(f, "  \"fullSizeIn128Resolution\": \"%s\",\n", epochData.FullSizeIn128Resolution.String())
+	_, err = fmt.Fprintf(f, "  \"branchDepth\": \"%s\",\n", epochData.BranchDepth.String())
+	_, err = fmt.Fprintf(f, "  \"merkleNodes\": ",)
+	writeElementsToFile(f, epochData.MerkleNodes)
+	_, err = fmt.Fprint(f, "\n}")
+	checkError(err)
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func writeElementsToFile(file *os.File, arr []*big.Int) {
+	_, err := fmt.Fprintf(file, "[")
+	checkError(err)
+	if len(arr) > 0 {
+		_, err = fmt.Fprintf(file, "\"%s\"", arr[0].String())
+		checkError(err)
+	}
+
+	for i := 1; i < len(arr); i++ {
+		_, err = fmt.Fprintf(file, ", \"%s\"", arr[i].String())
+		checkError(err)
+	}
+	_, err = fmt.Fprintf(file, "]")
+	checkError(err)
 }
