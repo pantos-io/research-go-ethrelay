9835586e6e3634a38d3449b3e1c2969f4a1f7095
Philipp Frauenthaler <p.frauenthaler@infosys.tuwien.ac.at>
2020-01-16 17:54:54 +0100

    buildDagTree with byte buffer

--- ethereum/ethash/block_meta_data.go
+++ ethereum/ethash/block_meta_data.go
@@ -22,6 +22,12 @@ type BlockMetaData struct {
 	DagTree     *mtree.DagTree
 }
 
+type BufferedDag struct {
+	Buf []byte
+	Indices []uint32
+	IndexCnt uint
+}
+
 func ProcessAllDuringRead(
 	datasetPath string, metaDataArray []*BlockMetaData) {
 	var f *os.File
@@ -59,11 +65,11 @@ func ProcessAllDuringRead(
 		if n != 128 {
 			log.Fatal("Malformed dataset")
 		}
+		fmt.Println("before write")
 		for _, metaData := range metaDataArray {
-			fmt.Println("before write")
 			metaData.DagTree.Insert(typedefs.Word(buf), i)
-			fmt.Println("after write")
 		}
+		fmt.Println("after write")
 		if err != nil && err != io.EOF {
 			log.Fatal(err)
 		}
@@ -115,6 +121,55 @@ func ProcessDuringRead(
 	}
 }
 
+func ProcessBufferedDag(bufferedDag *BufferedDag, mt *mtree.DagTree) {
+
+	for i:= uint(0); i < bufferedDag.IndexCnt; i++ {
+		buf := [128]byte{}
+		copy(buf[:], bufferedDag.Buf[i*128:(i+1)*128])
+		mt.Insert(typedefs.Word(buf), bufferedDag.Indices[i])
+	}
+	//var f *os.File
+	//var err error
+	//for {
+	//	f, err = os.Open(datasetPath)
+	//	if err == nil {
+	//		break
+	//	} else {
+	//		fmt.Printf("Reading DAG file %s failed with %s. Retry in 10s...\n", datasetPath, err.Error())
+	//		time.Sleep(10 * time.Second)
+	//	}
+	//}
+	//r := bufio.NewReader(f)
+	//buf := [128]byte{}
+	//// ignore first 8 bytes magic number at the beginning
+	//// of dataset. See more at https://gopkg.in/ethereum/wiki/wiki/Ethash-DAG-Disk-Storage-Format
+	//_, err = io.ReadFull(r, buf[:8])
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//var i uint32 = 0
+	//for {
+	//	n, err := io.ReadFull(r, buf[:128])
+	//	if n == 0 {
+	//		if err == nil {
+	//			continue
+	//		}
+	//		if err == io.EOF {
+	//			break
+	//		}
+	//		log.Fatal(err)
+	//	}
+	//	if n != 128 {
+	//		log.Fatal("Malformed dataset")
+	//	}
+	//	mt.Insert(typedefs.Word(buf), i)
+	//	if err != nil && err != io.EOF {
+	//		log.Fatal(err)
+	//	}
+	//	i++
+	//}
+}
+
 // make sure to only call with meta data of same epoch
 func BuildDagTrees(metaDataArray []*BlockMetaData) {
 	fmt.Println("step 1")
@@ -163,6 +218,26 @@ func (s *BlockMetaData) buildDagTree() {
 	s.DagTree.RegisterStoredLevel(uint32(branchDepth), uint32(10))
 	path := PathToDAG(uint64(s.blockNumber/30000), DefaultDir)
 	ProcessDuringRead(path, s.DagTree)
+	s.DagTree.Finalize()
+}
+
+func (s *BlockMetaData) BuildDagTree(dag *BufferedDag) {
+	indices := Instance.GetVerificationIndices(
+		s.blockNumber,
+		s.hashNoNonce,
+		s.nonce,
+	)
+	fmt.Printf("indices: %v\n", indices)
+	s.DagTree = mtree.NewDagTree()
+	s.DagTree.RegisterIndex(indices...)
+	//MakeDAG(s.blockNumber, DefaultDir)
+	fullSize := DAGSize(s.blockNumber)
+	fullSizeIn128Resolution := fullSize / 128
+	branchDepth := len(fmt.Sprintf("%b", fullSizeIn128Resolution-1))
+	s.DagTree.RegisterStoredLevel(uint32(branchDepth), uint32(10))
+	//path := PathToDAG(uint64(s.blockNumber/30000), DefaultDir)
+	//ProcessDuringRead(path, s.DagTree)
+	ProcessBufferedDag(dag, s.DagTree)
 	s.DagTree.Finalize()
 }
 
