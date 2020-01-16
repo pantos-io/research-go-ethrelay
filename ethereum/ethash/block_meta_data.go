3512a9a85b6df9fffefec97b1ca22b780746e132
Marten Sigwart <marten@okamaja.de>
2020-01-16 15:09:01 +0100

    expose method for batch processing of block meta data

--- ethereum/ethash/block_meta_data.go
+++ ethereum/ethash/block_meta_data.go
@@ -22,6 +22,52 @@ type BlockMetaData struct {
 	DagTree     *mtree.DagTree
 }
 
+func ProcessAllDuringRead(
+	datasetPath string, metaDataArray []*BlockMetaData) {
+	var f *os.File
+	var err error
+	for {
+		f, err = os.Open(datasetPath)
+		if err == nil {
+			break
+		} else {
+			fmt.Printf("Reading DAG file %s failed with %s. Retry in 10s...\n", datasetPath, err.Error())
+			time.Sleep(10 * time.Second)
+		}
+	}
+	r := bufio.NewReader(f)
+	buf := [128]byte{}
+	// ignore first 8 bytes magic number at the beginning
+	// of dataset. See more at https://gopkg.in/ethereum/wiki/wiki/Ethash-DAG-Disk-Storage-Format
+	_, err = io.ReadFull(r, buf[:8])
+	if err != nil {
+		log.Fatal(err)
+	}
+	var i uint32 = 0
+	for {
+		n, err := io.ReadFull(r, buf[:128])
+		if n == 0 {
+			if err == nil {
+				continue
+			}
+			if err == io.EOF {
+				break
+			}
+			log.Fatal(err)
+		}
+		if n != 128 {
+			log.Fatal("Malformed dataset")
+		}
+		for _, metaData := range metaDataArray {
+			metaData.DagTree.Insert(typedefs.Word(buf), i)
+		}
+		if err != nil && err != io.EOF {
+			log.Fatal(err)
+		}
+		i++
+	}
+}
+
 func ProcessDuringRead(
 	datasetPath string, mt *mtree.DagTree) {
 	var f *os.File
@@ -63,6 +109,31 @@ func ProcessDuringRead(
 			log.Fatal(err)
 		}
 		i++
+	}
+}
+
+// make sure to only call with meta data of same epoch
+func BuildDagTree(metaDataArray []*BlockMetaData) {
+	for _, s := range metaDataArray {
+		indices := Instance.GetVerificationIndices(
+			s.blockNumber,
+			s.hashNoNonce,
+			s.nonce,
+		)
+		fmt.Printf("indices: %v\n", indices)
+		s.DagTree = mtree.NewDagTree()
+		s.DagTree.RegisterIndex(indices...)
+		MakeDAG(s.blockNumber, DefaultDir)
+		fullSize := DAGSize(s.blockNumber)
+		fullSizeIn128Resolution := fullSize / 128
+		branchDepth := len(fmt.Sprintf("%b", fullSizeIn128Resolution-1))
+		s.DagTree.RegisterStoredLevel(uint32(branchDepth), uint32(10))
+	}
+
+	path := PathToDAG(metaDataArray[0].blockNumber/30000, DefaultDir)
+	ProcessAllDuringRead(path, metaDataArray)
+	for _, s := range metaDataArray {
+		s.DagTree.Finalize()
 	}
 }
 
