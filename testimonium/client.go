pantos-io/research-go-ethrelay

commit 0acf38c83a51ea66c99244b0e9dfb730528cc4a0

Updated go-ethereum

testimonium/client.go
@@ -52,9 +52,9 @@ type Client struct {
 }
 
 type Header struct {
-	Hash                      [32]byte
-	BlockNumber               *big.Int
-	TotalDifficulty           *big.Int
+	Hash            [32]byte
+	BlockNumber     *big.Int
+	TotalDifficulty *big.Int
 }
 
 type FullHeader struct {
@@ -268,7 +268,7 @@ func (c Client) Account() string {
 
 func (c Client) TotalBalance() (*big.Int, error) {
 	var totalBalance = new(big.Int)
-	for k, _ := range c.chains {
+	for k := range c.chains {
 		balance, err := c.Balance(k)
 		if err != nil {
 			return nil, err
@@ -279,7 +279,7 @@ func (c Client) TotalBalance() (*big.Int, error) {
 }
 
 func (c Client) Balance(chainId uint8) (*big.Int, error) {
-	var totalBalance = new(big.Int);
+	var totalBalance = new(big.Int)
 
 	_, exists := c.chains[chainId]
 	if !exists {
@@ -390,7 +390,7 @@ func (c Client) BlockHeaderExists(blockHash [32]byte, chain uint8) (bool, error)
 func (c Client) GetLongestChainEndpoint(chain uint8) ([32]byte, error) {
 	_, exists := c.chains[chain]
 	if !exists {
-		fmt.Errorf("chain %s does not exist", chain)
+		return [32]byte{}, fmt.Errorf("chain %s does not exist", chain)
 	}
 
 	return c.chains[chain].testimoniumContract.GetLongestChainEndpoint(nil)
@@ -404,7 +404,7 @@ func (c Client) GetOriginalBlockHeader(blockHash [32]byte, chain uint8) (*types.
 	return c.chains[chain].client.BlockByHash(context.Background(), common.BytesToHash(blockHash[:]))
 }
 
-func (c Client) SubmitHeader(header *types.Header, chain uint8) (error) {
+func (c Client) SubmitHeader(header *types.Header, chain uint8) error {
 	if _, exists := c.chains[chain]; !exists {
 		log.Fatalf("Chain '%d' does not exist", chain)
 	}
@@ -617,7 +617,7 @@ func (c Client) SubmitHeaderLive(destinationChain uint8, sourceChain uint8, lock
 	}
 }
 
-func (c Client) SubmitRLPHeader(rlpHeader []byte, chain uint8) (error) {
+func (c Client) SubmitRLPHeader(rlpHeader []byte, chain uint8) error {
 	// Check preconditions
 	if _, exists := c.chains[chain]; !exists {
 		log.Fatalf("Chain '%d' does not exist", chain)
@@ -671,7 +671,7 @@ func (c Client) SubmitRLPHeader(rlpHeader []byte, chain uint8) (error) {
 		// fmt.Printf("Tx successful: %s\n", eventIterator.Event.String())
 
 		// TODO: this is only 1 special hash value emitted by the contract for too small stake and not a read error code
-		if eventIterator.Event.BlockHash == [32] byte { 0 } {
+		if eventIterator.Event.BlockHash == [32]byte{0} {
 			return errors.New("block was not submitted, reason: too small stake deposited")
 		}
 
@@ -829,7 +829,7 @@ func getRlpHeaderByTestimoniumSubmitEvent(chain *Chain, blockHash [32]byte) ([]b
 			var parameter FunctionInputs
 
 			// unpack method inputs
-			err = method.Inputs.Unpack(&parameter, methodInputs)
+			err = testimoniumAbi.UnpackIntoInterface(&parameter, method.Name, methodInputs)
 			if err != nil {
 				return nil, err
 			}
@@ -949,22 +949,35 @@ func (c Client) GenerateMerkleProofForTx(txHash [32]byte, chain uint8) ([]byte, 
 	}
 
 	// create transactions trie
-	buffer := new(bytes.Buffer)
+	indexBuffer := new(bytes.Buffer)
+	txBuffer := new(bytes.Buffer)
 	merkleTrie := new(trie.Trie)
-	txList := block.Transactions()
-	for i := 0; i < txList.Len(); i++ {
-		buffer.Reset()
-		rlp.Encode(buffer, uint(i))
-		merkleTrie.Update(buffer.Bytes(), txList.GetRlp(i))
+	transactions := block.Transactions()
+
+	for i := 0; i < transactions.Len(); i++ {
+		indexBuffer.Reset()
+		txBuffer.Reset()
+		err = rlp.Encode(indexBuffer, uint(i))
+		if err != nil {
+			return []byte{}, []byte{}, []byte{}, []byte{}, err
+		}
+		transactions.EncodeIndex(i, txBuffer)
+		merkleTrie.Update(indexBuffer.Bytes(), txBuffer.Bytes())
 	}
 
-	// create Merkle proof
-	rlpEncodedTx := txList.GetRlp(int(txReceipt.TransactionIndex))
+	txBuffer.Reset()
+	transactions.EncodeIndex(int(txReceipt.TransactionIndex), txBuffer)
 
-	buffer.Reset()
-	rlp.Encode(buffer, txReceipt.TransactionIndex)
-	path := make([]byte, len(buffer.Bytes()))
-	copy(path, buffer.Bytes())
+	// create Merkle proof
+	rlpEncodedTx := txBuffer.Bytes()
+
+	indexBuffer.Reset()
+	err = rlp.Encode(indexBuffer, txReceipt.TransactionIndex)
+	if err != nil {
+		return []byte{}, []byte{}, []byte{}, []byte{}, err
+	}
+	path := make([]byte, len(indexBuffer.Bytes()))
+	copy(path, indexBuffer.Bytes())
 
 	merkleIterator := merkleTrie.NodeIterator(nil)
 	var proofNodes [][]byte
@@ -976,15 +989,21 @@ func (c Client) GenerateMerkleProofForTx(txHash [32]byte, chain uint8) ([]byte, 
 		}
 	}
 
-	buffer.Reset()
-	rlp.Encode(buffer, proofNodes)
-	rlpEncodedProofNodes := make([]byte, len(buffer.Bytes()))
-	copy(rlpEncodedProofNodes, buffer.Bytes())
+	indexBuffer.Reset()
+	err = rlp.Encode(indexBuffer, proofNodes)
+	if err != nil {
+		return []byte{}, []byte{}, []byte{}, []byte{}, err
+	}
+	rlpEncodedProofNodes := make([]byte, len(indexBuffer.Bytes()))
+	copy(rlpEncodedProofNodes, indexBuffer.Bytes())
 
-	buffer.Reset()
-	rlp.Encode(buffer, block.Header())
-	rlpEncodedHeader := make([]byte, len(buffer.Bytes()))
-	copy(rlpEncodedHeader, buffer.Bytes())
+	indexBuffer.Reset()
+	err = rlp.Encode(indexBuffer, block.Header())
+	if err != nil {
+		return []byte{}, []byte{}, []byte{}, []byte{}, err
+	}
+	rlpEncodedHeader := make([]byte, len(indexBuffer.Bytes()))
+	copy(rlpEncodedHeader, indexBuffer.Bytes())
 
 	return rlpEncodedHeader, rlpEncodedTx, path, rlpEncodedProofNodes, nil
 }
@@ -1073,17 +1092,17 @@ func (c Client) VerifyMerkleProof(feeInWei *big.Int, rlpHeader []byte, trieValue
 	auth := prepareTransaction(c.account, c.privateKey, c.chains[chain], feeInWei)
 
 	switch trieValueType {
-		case VALUE_TYPE_TRANSACTION:
-			tx, err = c.chains[chain].testimoniumContract.VerifyTransaction(auth, feeInWei, rlpHeader,
-				noOfConfirmations, rlpEncodedValue, path, rlpEncodedProofNodes)
-		case VALUE_TYPE_RECEIPT:
-			tx, err = c.chains[chain].testimoniumContract.VerifyReceipt(auth, feeInWei, rlpHeader, noOfConfirmations,
-				rlpEncodedValue, path, rlpEncodedProofNodes)
-		case VALUE_TYPE_STATE:
-			tx, err = c.chains[chain].testimoniumContract.VerifyState(auth, feeInWei, rlpHeader, noOfConfirmations,
-				rlpEncodedValue, path, rlpEncodedProofNodes)
-		default:
-			log.Fatal("Unexpected trie value type: ", trieValueType)
+	case VALUE_TYPE_TRANSACTION:
+		tx, err = c.chains[chain].testimoniumContract.VerifyTransaction(auth, feeInWei, rlpHeader,
+			noOfConfirmations, rlpEncodedValue, path, rlpEncodedProofNodes)
+	case VALUE_TYPE_RECEIPT:
+		tx, err = c.chains[chain].testimoniumContract.VerifyReceipt(auth, feeInWei, rlpHeader, noOfConfirmations,
+			rlpEncodedValue, path, rlpEncodedProofNodes)
+	case VALUE_TYPE_STATE:
+		tx, err = c.chains[chain].testimoniumContract.VerifyState(auth, feeInWei, rlpHeader, noOfConfirmations,
+			rlpEncodedValue, path, rlpEncodedProofNodes)
+	default:
+		log.Fatal("Unexpected trie value type: ", trieValueType)
 	}
 
 	if err != nil {
@@ -1107,12 +1126,12 @@ func (c Client) VerifyMerkleProof(feeInWei *big.Int, rlpHeader []byte, trieValue
 	var verificationResult *VerificationResult
 
 	switch trieValueType {
-		case VALUE_TYPE_TRANSACTION:
-			verificationResult, err = c.getVerifyTransactionEvent(chain, receipt)
-		case VALUE_TYPE_RECEIPT:
-			verificationResult, err = c.getVerifyReceiptEvent(chain, receipt)
-		case VALUE_TYPE_STATE:
-			verificationResult, err = c.getVerifyStateEvent(chain, receipt)
+	case VALUE_TYPE_TRANSACTION:
+		verificationResult, err = c.getVerifyTransactionEvent(chain, receipt)
+	case VALUE_TYPE_RECEIPT:
+		verificationResult, err = c.getVerifyReceiptEvent(chain, receipt)
+	case VALUE_TYPE_STATE:
+		verificationResult, err = c.getVerifyStateEvent(chain, receipt)
 	}
 
 	if err != nil {
@@ -1222,7 +1241,7 @@ func (c Client) SetEpochData(epochData typedefs.EpochData, chain uint8) {
 	}
 }
 
-func (c Client) DeployTestimonium(destinationChain uint8, sourceChain uint8, genesisBlockNumber uint64) (common.Address) {
+func (c Client) DeployTestimonium(destinationChain uint8, sourceChain uint8, genesisBlockNumber uint64) common.Address {
 	if _, exists := c.chains[destinationChain]; !exists {
 		log.Fatalf("DestinationChain chain '%d' does not exist", destinationChain)
 	}
@@ -1268,7 +1287,7 @@ func (c Client) DeployTestimonium(destinationChain uint8, sourceChain uint8, gen
 	return addr
 }
 
-func (c Client) DeployEthash(destinationChain uint8) (common.Address) {
+func (c Client) DeployEthash(destinationChain uint8) common.Address {
 	if _, exists := c.chains[destinationChain]; !exists {
 		log.Fatalf("DestinationChain chain '%d' does not exist", destinationChain)
 	}
@@ -1402,7 +1421,7 @@ func awaitTxReceipt(client *ethclient.Client, txHash common.Hash) (*types.Receip
 	receipts := make(chan *types.Receipt)
 
 	go func(chan *types.Receipt) {
-		for ; ; {
+		for {
 			receipt, _ := client.TransactionReceipt(context.Background(), txHash)
 
 			if receipt != nil {

ethereum/ethash/EthashContract.go
@@ -154,7 +154,7 @@ func bindEthash(address common.Address, caller bind.ContractCaller, transactor b
 // sets the output to result. The result type might be a single field for simple
 // returns, a slice of interfaces for anonymous returns and a struct for named
 // returns.
-func (_Ethash *EthashRaw) Call(opts *bind.CallOpts, result interface{}, method string, params ...interface{}) error {
+func (_Ethash *EthashRaw) Call(opts *bind.CallOpts, result *[]interface{}, method string, params ...interface{}) error {
 	return _Ethash.Contract.EthashCaller.contract.Call(opts, result, method, params...)
 }
 
@@ -173,7 +173,7 @@ func (_Ethash *EthashRaw) Transact(opts *bind.TransactOpts, method string, param
 // sets the output to result. The result type might be a single field for simple
 // returns, a slice of interfaces for anonymous returns and a struct for named
 // returns.
-func (_Ethash *EthashCallerRaw) Call(opts *bind.CallOpts, result interface{}, method string, params ...interface{}) error {
+func (_Ethash *EthashCallerRaw) Call(opts *bind.CallOpts, result *[]interface{}, method string, params ...interface{}) error {
 	return _Ethash.Contract.contract.Call(opts, result, method, params...)
 }
 
@@ -192,12 +192,17 @@ func (_Ethash *EthashTransactorRaw) Transact(opts *bind.TransactOpts, method str
 //
 // Solidity: function isEpochDataSet(uint256 epochIndex) view returns(bool)
 func (_Ethash *EthashCaller) IsEpochDataSet(opts *bind.CallOpts, epochIndex *big.Int) (bool, error) {
-	var (
-		ret0 = new(bool)
-	)
-	out := ret0
-	err := _Ethash.contract.Call(opts, out, "isEpochDataSet", epochIndex)
-	return *ret0, err
+	var out []interface{}
+	err := _Ethash.contract.Call(opts, &out, "isEpochDataSet", epochIndex)
+
+	if err != nil {
+		return *new(bool), err
+	}
+
+	out0 := *abi.ConvertType(out[0], new(bool)).(*bool)
+
+	return out0, err
+
 }
 
 // IsEpochDataSet is a free data retrieval call binding the contract method 0xc7b81f4f.
@@ -218,16 +223,18 @@ func (_Ethash *EthashCallerSession) IsEpochDataSet(epochIndex *big.Int) (bool, e
 //
 // Solidity: function verifyPoW(uint256 blockNumber, bytes32 rlpHeaderHashWithoutNonce, uint256 nonce, uint256 difficulty, uint256[] dataSetLookup, uint256[] witnessForLookup) view returns(uint256, uint256)
 func (_Ethash *EthashCaller) VerifyPoW(opts *bind.CallOpts, blockNumber *big.Int, rlpHeaderHashWithoutNonce [32]byte, nonce *big.Int, difficulty *big.Int, dataSetLookup []*big.Int, witnessForLookup []*big.Int) (*big.Int, *big.Int, error) {
-	var (
-		ret0 = new(*big.Int)
-		ret1 = new(*big.Int)
-	)
-	out := &[]interface{}{
-		ret0,
-		ret1,
+	var out []interface{}
+	err := _Ethash.contract.Call(opts, &out, "verifyPoW", blockNumber, rlpHeaderHashWithoutNonce, nonce, difficulty, dataSetLookup, witnessForLookup)
+
+	if err != nil {
+		return *new(*big.Int), *new(*big.Int), err
 	}
-	err := _Ethash.contract.Call(opts, out, "verifyPoW", blockNumber, rlpHeaderHashWithoutNonce, nonce, difficulty, dataSetLookup, witnessForLookup)
-	return *ret0, *ret1, err
+
+	out0 := *abi.ConvertType(out[0], new(*big.Int)).(**big.Int)
+	out1 := *abi.ConvertType(out[1], new(*big.Int)).(**big.Int)
+
+	return out0, out1, err
+
 }
 
 // VerifyPoW is a free data retrieval call binding the contract method 0x29e265df.
@@ -407,5 +414,6 @@ func (_Ethash *EthashFilterer) ParseSetEpochData(log types.Log) (*EthashSetEpoch
 	if err := _Ethash.contract.UnpackLog(event, "SetEpochData", log); err != nil {
 		return nil, err
 	}
+	event.Raw = log
 	return event, nil
 }

go.mod
@@ -3,31 +3,18 @@ module github.com/pantos-io/go-ethrelay
 go 1.13
 
 require (
-	github.com/VictoriaMetrics/fastcache v1.5.5 // indirect
 	github.com/allegro/bigcache v1.2.1 // indirect
-	github.com/aristanetworks/goarista v0.0.0-20191206003309-5d8d36c240c9 // indirect
-	github.com/btcsuite/btcd v0.20.1-beta // indirect
+	github.com/btcsuite/btcd v0.22.0-beta // indirect
 	github.com/cespare/cp v1.1.1 // indirect
 	github.com/deckarep/golang-set v1.7.1 // indirect
 	github.com/edsrzf/mmap-go v1.0.0
-	github.com/elastic/gosigar v0.10.5 // indirect
-	github.com/ethereum/go-ethereum v1.9.9
-	github.com/fjl/memsize v0.0.0-20190710130421-bcb5799ab5e5 // indirect
+	github.com/ethereum/go-ethereum v1.10.8
 	github.com/gballet/go-libpcsclite v0.0.0-20191108122812-4678299bea08 // indirect
-	github.com/google/uuid v1.1.1 // indirect
-	github.com/gorilla/websocket v1.4.1 // indirect
-	github.com/hashicorp/golang-lru v0.5.3 // indirect
-	github.com/huin/goupnp v1.0.0 // indirect
 	github.com/jackpal/go-nat-pmp v1.0.2 // indirect
 	github.com/karalabe/usb v0.0.0-20191104083709-911d15fe12a9 // indirect
-	github.com/mattn/go-colorable v0.1.2 // indirect
-	github.com/mattn/go-isatty v0.0.9 // indirect
-	github.com/olekukonko/tablewriter v0.0.4 // indirect
-	github.com/pborman/uuid v1.2.0 // indirect
 	github.com/pelletier/go-toml v1.6.0 // indirect
 	github.com/prometheus/tsdb v0.10.0 // indirect
 	github.com/rjeczalik/notify v0.9.2 // indirect
-	github.com/rs/cors v1.7.0 // indirect
 	github.com/spf13/afero v1.2.2 // indirect
 	github.com/spf13/cast v1.3.1 // indirect
 	github.com/spf13/cobra v0.0.5
@@ -36,10 +23,7 @@ require (
 	github.com/spf13/viper v1.6.1
 	github.com/status-im/keycard-go v0.0.0-20200107115650-f38e9a19958e // indirect
 	github.com/tyler-smith/go-bip39 v1.0.2 // indirect
-	golang.org/x/crypto v0.0.0-20191227163750-53104e6ec876
-	golang.org/x/net v0.0.0-20191209160850-c0dbc17a3553 // indirect
-	golang.org/x/sys v0.0.0-20200107162124-548cf772de50 // indirect
+	golang.org/x/crypto v0.0.0-20210920023735-84f357641f63
+	golang.org/x/sys v0.0.0-20210921065528-437939a70204 // indirect
 	gopkg.in/ini.v1 v1.51.1 // indirect
-	gopkg.in/olebedev/go-duktape.v3 v3.0.0-20190709231704-1e4459ed25ff // indirect
-	gopkg.in/yaml.v2 v2.2.7 // indirect
 )

commit fc7b98afdae853eaead8572adb55d85972dceb90

Fix Merkle Proof verification
Regression introduced in 0acf38c83a51ea66c99244b0e9dfb730528cc4a0

testimonium/client.go
@@ -903,7 +903,12 @@ func (c Client) GenerateMerkleProofForTx(chainId string, txHash common.Hash) ([]
 			return []byte{}, []byte{}, []byte{}, []byte{}, err
 		}
 		transactions.EncodeIndex(i, txBuffer)
-		merkleTrie.Update(indexBuffer.Bytes(), txBuffer.Bytes())
+
+		// Trie.Update() requires immutability of the value as long as it's stored in the data structure
+		encodedTx := make([]byte, txBuffer.Len())
+		copy(encodedTx, txBuffer.Bytes())
+
+		merkleTrie.Update(indexBuffer.Bytes(), encodedTx)
 	}
 
 	txBuffer.Reset()